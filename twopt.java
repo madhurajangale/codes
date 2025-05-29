136120cfe45a5aa231313d15f22c227be719f566
Madhura Jangale <142075673+madhurajangale@users.noreply.github.com>
2025-05-29 11:58:15 +0530

    Create twopt.java

---
+++ twopt.java
@@ -0,0 +1,17 @@
+//Two Pointers
+//leetcode 905
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int l = 0;
+        for(int r=0; r<nums.length; r++){
+            if(nums[r]%2==0){
+                int temp=nums[r];
+                nums[r]=nums[l];
+                nums[l]=temp;
+                l++;
+            }
+        }
+        return nums;
+    }
+}
